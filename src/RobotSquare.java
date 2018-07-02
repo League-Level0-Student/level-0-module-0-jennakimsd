b9601ac00a78c58c7a7e5bf15db84492e0776cf6
jennakimsd <league@WTS-IM-17.attlocal.net>
2018-07-02 16:54:33 -0700

    I did greeter and robot square

--- src/RobotSquare.java
+++ src/RobotSquare.java
@@ -11,22 +11,26 @@ public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
     	// 1. Make a new Robot
-
+    	
+    	Robot sami=new Robot();
 
         // 3. Put the robot's pen down
-
+sami.penDown();
+sami.setRandomPenColor();
 
         // 6. Make the robot move as fast as possible
-
+sami.setSpeed(500);
 
         // 5. Do everything below here 4 times
+for (int i = 0; i < 4; i++) {
+	
 
 
         //         2. Move your robot 200 pixels
-
+sami.move(200);
 
         //         4. Turn the robot 90 degrees to the right (90 degrees)
+sami.turn(90);
 
+    }}}
 
-    }
-}
